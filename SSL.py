7f7e245f970babda9c93bc68f2695df4a747bcb4
ByteForge <46260031+ByteForge786@users.noreply.github.com>
2024-12-04 17:20:08 +0530

    Update SSL.py

--- SSL.py
+++ SSL.py
@@ -1,15 +1,32 @@
+import os
 import requests
+import urllib3
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-# Patch the original Session class
-original_session = requests.Session
-class PatchedSession(original_session):
-    def __init__(self):
-        super().__init__()
-        self.verify = False
-        
-requests.Session = PatchedSession
+# Disable all warnings related to unsecure requests
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# Now import and use jiraone
-from jiraone import LOGIN, endpoint
+# Create a custom adapter with SSL verification disabled
+class SSLAdapter(requests.adapters.HTTPAdapter):
+    def init_poolmanager(self, *args, **kwargs):
+        kwargs['ssl_context'] = urllib3.util.ssl_.create_urllib3_context()
+        kwargs['assert_hostname'] = False
+        kwargs['cert_reqs'] = 'CERT_NONE'
+        return super().init_poolmanager(*args, **kwargs)
+
+# Create custom session
+session = requests.Session()
+adapter = SSLAdapter()
+session.mount('https://', adapter)
+session.verify = False
+
+# Import jiraone after setting up the session
+from jiraone import LOGIN
+
+# Use the custom session when initializing LOGIN
+LOGIN(
+    user="your_email",
+    password="your_token", 
+    url="your_jira_url",
+    session=session
+)
